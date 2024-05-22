1f6f93dce2250577a69c00a7300cd4367d09cd8b
JustOksu <100164789+JustOksu@users.noreply.github.com>
2024-05-22 10:00:59 +0000

    Deploying to gh-pages from @ JustOksu/goit-js-hw-10@e67512715574eab4a31bdb46c0f3ad4f588dd1d2 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",function(o){o.preventDefault();const t=parseInt(this.elements.delay.value),s=this.elements.state.value;new Promise((e,r)=>{s==="fulfilled"?setTimeout(()=>{e(t)},t):s==="rejected"&&setTimeout(()=>{r(t)},t)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers3.js.map
